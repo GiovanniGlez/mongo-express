4d9acdaf56212bca06b19741b2829fcb7517b155
Gio <giovannigonzalezdelgado25@gmail.com>
2022-08-20 00:19:11 -0500

    feat: variables de entorno :)

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const mongoose = require ("mongoose")
+const express = require ("express")
+
+const app = express()
+
+//Destructuración
+const {
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_NAME
+} = process.env
+
+//URL
+const URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}${DB_HOST}${DB_NAME}`
+
+//Conectando a la base de datos
+mongoose.connect (URL)
+.then(() => {
+    console.log("Conectado a la base de datos de mongo")
+
+    //Levantar servidor
+    app.listen(8080, () => {
+        console.log("Server listening...")
+    })
+})
+.catch((Err) => {
+    console.log("Error", Err)
+})
